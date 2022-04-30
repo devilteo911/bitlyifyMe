57a7c5bc749b8716182204d9cf085757b1276a05
Matteo <devilteo911>
2022-04-30 13:02:30 +0200

    ✨ better input control

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import warnings
-from utils import driver_options
+from utils import driver_options, split_multi_link
 
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 
 url = "https://bitly.com/"
 options = driver_options(webdriver)
-
 
 # send over the link
 
@@ -23,16 +22,19 @@ driver = webdriver.Chrome(executable_path=os.getcwd() + "/chromedriver.exe", opt
 
 while True:
     input_url = input('URL to shrink: ')
-    driver.get(url)
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located((By.ID, "unAuthShortenForm")))
+    links = split_multi_link(input_url)
+    for link in links:
+        driver.get(url)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.presence_of_element_located((By.ID, "unAuthShortenForm")))
 
-    input_bar = driver.find_element(By.XPATH, "//*[@id=\"shorten_url\"]")
-    submit_button = driver.find_element(By.ID, "shorten_btn")
+        input_bar = driver.find_element(By.XPATH, "//*[@id=\"shorten_url\"]")
+        submit_button = driver.find_element(By.ID, "shorten_btn")
 
-    input_bar.send_keys(input_url)
-    submit_button.click()
+        input_bar.send_keys(link)
+        submit_button.click()
 
-    time.sleep(1)
-    result = driver.find_element(By.CLASS_NAME, "short-link").text
-    print(f"\nResult: {result}")
+        time.sleep(1)
+        result = driver.find_element(By.CLASS_NAME, "short-link").text
+        print(f"\nOriginal: {link} -> Shrunk: {result}")
+    print("\n")
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def driver_options(webdriver):
     options = webdriver.ChromeOptions()
     options.headless = True
@@ -5,3 +8,15 @@ def driver_options(webdriver):
     options.add_argument("--log-level=3")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     return options
+
+def split_multi_link(in_str: str) -> list:
+    in_str = string_normalizer(in_str)
+    in_str = in_str.split(" ")
+    in_str = list(filter(None, in_str))
+    return in_str
+
+def string_normalizer(str_: str) -> str:
+    banned_char = ",;/'#@"
+    for bc in banned_char:
+        str_ = str_.replace(bc, " ")
+    return str_
